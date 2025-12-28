3869986f62e277b7a206fe128f7e848597e7aaba
Ioana Diana Ghisoiu <ioanagh077@gmail.com>
2025-12-28 17:44:00 +0200

    partea de dictionare

--- config_check.sh
+++ config_check.sh
@@ -1,27 +1,74 @@
 #!/bin/bash
 
-file="$1"
+file=$1
 
-#verificare daca file a primit un argument?
-if [ -z "$file" ]; then
-	echo " Error, input format: ./config_check <filename>"
+
+if [[ -z "$file" ]]; then
+	echo "eroare: te rog introdu calea catre un fisier"
 	exit 1
+fi
+
+
+
+if [[ ! -f "$file" ]]; then
+	echo "fisierul '$file' nu exista"
+	exit 1
+fi
+
+
+
+if grep -qi "PermitRootLogin yes" "$file"; then
+	echo "PERICOL:  root login este activat"
+else
+	echo "root login este dezactivat sau neschimbat"
 fi
 
 #verificarea permisiunilor
 permisiuni=$(stat -c  "%a" "$file")
 if [ "$permisiuni" -ne 600 ] && [ "$permisiuni" -ne 644 ]; then
  	echo "Permisiuni nesigure, se recomanda: 600"
-	exit 1
 fi
 
-#citirea fisierului ssh
-while read -r line; do
-	if [[ -z "$line" || "$line" == \#* ]]; then
+declare -A dictionar
+curent_host="global"
+
+
+#citirea fisierului ssh si folosirea unui dictionar pentru a memora datele 
+while read -r linie || [ -n "$linie" ]; do
+
+	#ignora spatiile si comentariile din fisier
+	if [[ -z "$linie" || "$linie" == \#* ]]; then
 		continue
- 	fi
-	echo "$line"
-	myarray+=($line)
+	fi
+
+	#elimina spatiile suplimentare in asa fel incat sa avem doar un spatiu intre cele 2 cuvinte
+	linie=$(echo "$linie" | xargs)
+
+
+	#punem in current_host numele hostului curent
+	if [[ "$linie" == Host\ * ]]; then
+        	curent_host=$(echo "$linie" | awk '{print $2}')
+        	continue
+    	fi
+
+
+   	 # Extragem cheia (ex: Port, User, PasswordAuthentication)
+    	cheie=$(echo "$linie" | awk '{print $1}')       #cheie=hostname
+    	valoare=$(echo "$linie" | awk '{print $2}')     #valoare = 4
+    	cheie_dictionar="${curent_host}_${cheie}"      # cheia_ dictionar = alexia_hostname
+
+    # 1. VERIFICARE DUPLICATE (Pentru orice setare)
+    if [[ -n "${dictionar[$cheie_dictionar]}" ]]; then
+        echo "[DUPLICAT] La host-ul $curent_host, opțiunea $cheie este deja setată!"
+    else
+        # Stocăm valoarea (salvăm totul pentru a putea verifica ulterior)
+        dictionar["$cheie_dictionar"]="$valoare"
+    fi
+
+   
 done < "$file" 
 
 
+for cheie_compusa in "${!dictionar[@]}"; do
+    echo   "$cheie_compusa - ${dictionar[$cheie_compusa]}"
+done 
