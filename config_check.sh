bb006e1e4a376056720785ac3bada579206f7087
alexiapaunete-cyber <alexia.paunete@gmail.com>
2026-01-03 15:51:56 +0200

    config_check.sh

--- config_check.sh
+++ config_check.sh
@@ -10,32 +10,70 @@ if [[ ! -f "$file" ]]; then
 	exit 1
 fi
 
-if grep -qi "PermissionRootLogin yes" "$file"; then
-	echo "PERICOL:  root login este activat"
-else
-	echo "root login este dezactivat sau neschimbat"
-fi
 
 permisiuni=$(stat -c %a "$file")
 if [[ "$permisiuni" != 600 || "$permisiuni" != 644  ]]; then
 	echo "atentie: permisiuni nesigure ($permisiuni); se recomanda 600"
 fi
 
-while read -r line; do
+declare -A dictionar
+curent_host="global"
+
+
+while read -r line || [ -n "$line" ]; do
 	if [[ -z "$line" || "$line"==\#* ]]; then
 		continue
 	fi
-	if [[ "$line"=="Host "* ]]; then
-		nume=$(echo $line | awk '{print $2}')
-		if [[ -n "$nume" ]]; then
-			dictionar["Host"] = $nume
-		fi
-	else
-		cheie = $(echo $line | awk '{print $1}')
-		valoare = $(echo $line | awk '{print $2}')
-		dictionar["$cheie"] = $valoare
+
+
+	line=$(echo "$line" | xargs)
+
+
+	if [[ "$line" == Host\ * ]]; then
+		curent_host=$(echo "$line" | awk '{print $2}')
+		continue
 	fi
-for $cheie in "${!dictionar[@]}"; do
-	echo "$cheie are valoarea: ${dictionar[2]}"
-done
+
+	cheie=$(echo "$line" | awk '{print $1}')
+	valoare=$(echo "$line" | awk '{print $2}')
+	cheie_dictionar="${curent_host}_${cheie}"
+
+	if [[ -n "${dictionar[$cheie_dictionar]}" ]]; then
+		echo "DUPLICAT la host-ul $curent_host, optiunea $cheie e deja setata"
+	else
+		dictionar["$cheie_dictionar"]="$valoare"
+
+		case "$cheie" in
+			"PermitRootLogin")
+				if [[ "$valoare" == "yes" ]]; then
+					echo "host: $curent_host -> root login este activat"
+				fi
+				;;
+			"PasswordAuthentication")
+				if [[ "$valoare" == "yes" ]]; then
+                        	        echo "host: $curent_host -> permite parole; recomandat: 'no' (folositi chei)"
+				fi
+                	        ;;
+			"PermitEmptyPasswords")
+				if [[ "$valoare" == "yes" ]]; then
+        	                        echo "host: $curent_host -> permite parole goale"
+                	        fi
+            	    	        ;;
+			"Port")
+				if [[ "$valoare" == "yes" ]]; then
+                	                echo "host: $curent_host -> foloseste portul standard 22"
+                	        fi
+                	        ;;
+		esac
+	fi
+
+
 done < "$file"
+
+
+for cheie_compusa in "${!dictionar[@]}"; do
+        echo "$cheie_compusa - ${dictionar[$cheie_compusa]}"
+done
+
+
+
