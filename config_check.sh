a04b280859b8602887799134cb3210b2abf971d7
Ioana Diana Ghisoiu <ioanagh077@gmail.com>
2026-01-05 11:53:54 +0200

    verificarea securitatii

--- config_check.sh
+++ config_check.sh
@@ -23,7 +23,7 @@ else
 	echo "root login este dezactivat sau neschimbat"
 fi
 
-#verificarea permisiunilor
+
 permisiuni=$(stat -c  "%a" "$file")
 if [ "$permisiuni" -ne 600 ] && [ "$permisiuni" -ne 644 ]; then
  	echo "Permisiuni nesigure, se recomanda: 600"
@@ -33,36 +33,61 @@ declare -A dictionar
 curent_host="global"
 
 
-#citirea fisierului ssh si folosirea unui dictionar pentru a memora datele 
+
 while read -r linie || [ -n "$linie" ]; do
 
-	#ignora spatiile si comentariile din fisier
+	
 	if [[ -z "$linie" || "$linie" == \#* ]]; then
 		continue
 	fi
 
-	#elimina spatiile suplimentare in asa fel incat sa avem doar un spatiu intre cele 2 cuvinte
+	
 	linie=$(echo "$linie" | xargs)
 
 
-	#punem in current_host numele hostului curent
 	if [[ "$linie" == Host\ * ]]; then
         	curent_host=$(echo "$linie" | awk '{print $2}')
         	continue
     	fi
 
 
-   	 # Extragem cheia (ex: Port, User, PasswordAuthentication)
+   	 
     	cheie=$(echo "$linie" | awk '{print $1}')       #cheie=hostname
     	valoare=$(echo "$linie" | awk '{print $2}')     #valoare = 4
     	cheie_dictionar="${curent_host}_${cheie}"      # cheia_ dictionar = alexia_hostname
 
-    # 1. VERIFICARE DUPLICATE (Pentru orice setare)
+    
     if [[ -n "${dictionar[$cheie_dictionar]}" ]]; then
         echo "[DUPLICAT] La host-ul $curent_host, opțiunea $cheie este deja setată!"
     else
-        # Stocăm valoarea (salvăm totul pentru a putea verifica ulterior)
+        
         dictionar["$cheie_dictionar"]="$valoare"
+
+	case "$cheie" in
+		"PermitRootLogin")
+         		if [[ "$valoare" == "yes" ]]; then
+				echo "host: $curent_host -> root login este activat"
+			fi
+			;;
+		"PasswordAuthentication")
+			if [[ "$valoare" == "yes" ]]; then
+                        	 echo "host: $curent_host -> permite parole; recomandat: 'no' (folositi chei)"
+			fi
+                        ;;
+		"PermitEmptyPasswords")
+			if [[ "$valoare" == "yes" ]]; then
+        	                 echo "host: $curent_host -> permite parole goale"
+               	        fi
+          	    	;;
+		"Port")
+			if [[ "$valoare" == "yes" ]]; then
+               	                echo "host: $curent_host -> foloseste portul standard 22"
+               	        fi
+               	        ;;
+	esac
+
+
+
     fi
 
    
