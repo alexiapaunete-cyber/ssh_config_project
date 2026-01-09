01e588d82b9449bc85a279f399dcb60d016cc014
alexiapaunete-cyber <alexia.paunete@gmail.com>
2026-01-09 19:04:24 +0200

    verificarea suprascriselor si a duplicatelor

--- config_check.sh
+++ config_check.sh
@@ -1,79 +1,145 @@
 #!/bin/bash
+
 file=$1
+
+
 if [[ -z "$file" ]]; then
-	echo "eroare: te rog introdu calea catre un fisier"
-	exit 1
+        echo "eroare: te rog introdu calea catre un fisier"
+        exit 1
 fi
+
+
 
 if [[ ! -f "$file" ]]; then
-	echo "fisierul '$file' nu exista"
-	exit 1
+        echo "eroare: fisierul '$file' nu exista"
+        exit 1
 fi
 
 
-permisiuni=$(stat -c %a "$file")
-if [[ "$permisiuni" != 600 || "$permisiuni" != 644  ]]; then
-	echo "atentie: permisiuni nesigure ($permisiuni); se recomanda 600"
+permisiuni=$(stat -c  "%a" "$file")
+if [ "$permisiuni" -ne 600 ] && [ "$permisiuni" -ne 644 ]; then
+        echo "permisiuni nesigure, se recomanda: 600"
+fi
+
+proprietar=$(stat -c "%U" "$file")
+if [ "$proprietar" != "root" ]; then
+        echo -e "fisierul nu este detinut de root! proprietar actual: $proprietar"
 fi
 
 declare -A dictionar
-curent_host="global"
+setare="global"
 
 
-while read -r line || [ -n "$line" ]; do
-	if [[ -z "$line" || "$line" == \#* ]]; then
-		continue
-	fi
+
+while read -r linie || [ -n "$linie" ]; do
 
 
-	line=$(echo "$line" | xargs)
+        if [[ -z "$linie" || "$linie" == \#* ]]; then
+                continue
+        fi
 
 
-	if [[ "$line" == Host\ * ]]; then
-		curent_host=$(echo "$line" | awk '{print $2}')
-		continue
-	fi
+        linie=$(echo "$linie" | xargs)
 
-	cheie=$(echo "$line" | awk '{print $1}')
-	valoare=$(echo "$line" | awk '{print $2}')
-	cheie_dictionar="${curent_host}_${cheie}"
 
-	if [[ -n "${dictionar[$cheie_dictionar]}" ]]; then
-		echo "DUPLICAT: la host-ul $curent_host, optiunea $cheie e deja setata"
-	else
-		dictionar["$cheie_dictionar"]="$valoare"
+        if [[ "$linie" == Match\ * ]]; then
+                continue
+        fi
 
-		case "$cheie" in
-			"PermitRootLogin")
-				if [[ "$valoare" == "yes" ]]; then
-					echo "host: $curent_host -> root login este activat"
-				fi
-				;;
-			"PasswordAuthentication")
-				if [[ "$valoare" == "yes" ]]; then
-                        	        echo "host: $curent_host -> permite parole; recomandat: 'no' (folositi chei)"
-				fi
-                	        ;;
-			"PermitEmptyPasswords")
-				if [[ "$valoare" == "yes" ]]; then
-        	                        echo "host: $curent_host -> permite parole goale"
-                	        fi
-            	    	        ;;
-			"Port")
-				if [[ "$valoare" == "yes" ]]; then
-                	                echo "host: $curent_host -> foloseste portul standard 22"
-                	        fi
-                	        ;;
-		esac
-	fi
+        cheie=$(echo "$linie" | awk '{print $1}')
+        valoare=$(echo "$linie" | awk '{print $2}')
+        cheie_dictionar="${setare}_${cheie}"
+
+    if [[ -n "${dictionar[$cheie_dictionar]}" ]]; then
+        if [[ "${dictionar[$cheie_dictionar]}" == "$valore" ]]; then
+                 echo "[DUPLICAT] in sectiunea $setare, optiunea $cheie apare de mai multe ori cu aceeasi valoare!"
+        else
+                echo"[SUPRASCRIERE] in sectiunea $setare se atribuie mai multe valori diferite!"
+        fi
+    else
+
+        dictionar["$cheie_dictionar"]="$valoare"
+
+     case "$cheie" in
+                "PermitRootLogin")
+                        if [[ "$valoare" != "no" ]]; then
+                                echo "$setare: PermitRootLogin este $valoare, se recomanda no  "
+                        fi
+                        ;;
+                "PasswordAuthentication")
+                        if [[ "$valoare" != "no" ]]; then
+                                 echo " $setare: permite parole, recomandat: no (folositi chei ssh)"
+                        fi
+                        ;;
+                "PermitEmptyPasswords")
+                        if [[ "$valoare" != "no" ]]; then
+                                 echo "$setare: permite parole goale"
+                        fi
+                        ;;
+                "Port")
+                        if [[ "$valoare" == "22" ]]; then
+                                echo " $setare: foloseste portul standard 22, schimbati-l pentru a reduce atacurile bot "
+                        fi
+                        ;;
+                "X11Forwarding")
+                        if [[ "$valoare" != "no" ]]; then
+                                echo " X11Forwarding este activat, daca nu folositi aplicatii grafice se recomanda dezactivarea sa"
+                        fi
+                        ;;
+                "MaxAuthTries")
+                        if [ "$valoare" -gt 3 ]; then
+                                echo "MaxAuthTries este $valoare, este recomandat sa fie <=3"
+                        fi
+                        ;;
+        esac
+
+
+
+    fi
 
 
 done < "$file"
 
 
-for cheie_compusa in "${!dictionar[@]}"; do
-        echo "$cheie_compusa - ${dictionar[$cheie_compusa]}"
+
+declare -A recomandari
+recomandari["PermitRootLogin"]="no"
+recomandari["PasswordAuthentication"]="no"
+recomandari["PermitEmptyPasswords"]="no"
+recomandari["MaxAuthTries"]="3"
+recomandari["X11Forwarding"]="no"
+
+
+printf "%s\n" "${!recomandari[@]}" | while read -r i; do
+        if [[ -z "$i" ]]; then
+                 continue
+        fi
+
+         valoare_ideala="${recomandari[$i]}"
+         valoare_curenta="${dictionar[global_$i]}"
+
+         if [[ -n "$valoare_curenta" ]]; then
+         if [[ "$valoare_curenta" != "$valoare_ideala" ]]; then
+                 echo " $i are  valoarea $valoare_curenta, trebuie schimbata in $valoare_ideala!"
+         fi
+         else
+                linie_comentata=$(grep "#$i" "$file" | head -n 1)
+                valoare_comentata=$(echo "$linie_comentata" | awk '{print $2}')
+
+                if [[ -n "$valoare_comentata" ]]; then
+                         if [[ "$valoare_comentata" != "$valoare_ideala" ]]; then
+                                 echo "atentie $i este comentata cu valoarea $valoare_comentata, valoarea default a sistemului este nesigura, se recomanda $i $valoare_ideala "
+                         else
+                                 echo " $i este comentata, dar are valoarea default sigura $valoare_comentata "
+                         fi
+                else
+                         echo "lipseste '$i' nu apare deloc, se recomanda adaugarea liniei $i $valoare_ideala "
+                fi
+         fi
 done
 
 
 
+for cheie_compusa in "${!dictionar[@]}"; do
+    echo   "$cheie_compusa - ${dictionar[$cheie_compusa]}"
+done
